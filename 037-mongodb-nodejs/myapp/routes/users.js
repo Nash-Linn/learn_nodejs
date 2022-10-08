549b40c93da008cb2302313bd19aed0ffbcf2b37
linnc <nash0207@163.com>
2022-10-08 17:17:45 +0800

    update

--- 037-mongodb-nodejs/myapp/routes/users.js
+++ 037-mongodb-nodejs/myapp/routes/users.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const UserModel = require("../model/UserModel");
 var router = express.Router();
 
 /* GET users listing. */
@@ -10,12 +11,46 @@ router.post("/user/add", (req, res) => {
   console.log("req.body", req.body);
 
   //插入数据库
-  //1.创建一个模型（user），————对应数据库的集合（users）
+  //1.创建一个模型（user,限制file类型），————对应数据库的集合（users）
   //user.create  user.find  user.delect user.update
 
-  res.send({
-    ok: 1,
+  const { username, password, age } = req.body;
+  UserModel.create({
+    username,
+    password,
+    age,
+  }).then((data) => {
+    console.log("data", data);
+    res.send({
+      ok: 1,
+    });
   });
+});
+
+//动态路由，获取id
+router.post("/user/update/:id", (req, res) => {
+  const { username, password, age } = req.body;
+  //第一项是匹配的字段  第二项是要更新的内容
+  UserModel.updateOne(
+    { _id: req.params.id },
+    {
+      username,
+      password,
+      age,
+    }
+  )
+    .then((data) => {
+      console.log("data", data);
+      res.send({
+        ok: 1,
+      });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.send({
+        ok: 0,
+      });
+    });
 });
 
 module.exports = router;
